21d109aaa2d9cc1aa6d737ed57f6d735b7a614a0
Augustine <augustine.kim@griffithuni.edu.au>
2024-05-24 01:28:46 +1000

    update welcomeblade and logo

--- WIL_project/resources/views/layouts/navigation.blade.php
+++ WIL_project/resources/views/layouts/navigation.blade.php
@@ -7,7 +7,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+
+                    <!-- <img src="{{ asset('images/WIL_LOGO.png') }}" class="block h-11 w-auto" alt="WIL Logo"> -->
+                        <x-application-logo class="block h-2 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
